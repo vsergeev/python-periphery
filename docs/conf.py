d852f59fd9ab20e85db7e23bbd95ba7563d52731
Vanya Sergeev <vsergeev@gmail.com>
2015-06-24 06:38:39 -0700

    docs: add author to sphinx LaTeX configuration

--- docs/conf.py
+++ docs/conf.py
@@ -234,7 +234,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'periphery.tex', u'python-periphery Documentation',
-   u'Author', 'manual'),
+   u'Vanya Sergeev', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
