722ac3c08d7f7188bf61ab0bd04a6c0b7d7b275a
Joseph Kogut <joseph.kogut@gmail.com>
2016-09-02 15:25:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='python-periphery',
-    version='1.0.0',
-    description='A pure Python 2/3 library for peripheral I/O (GPIO, SPI, I2C, MMIO, Serial) in Linux.',
+    version='1.1.0',
+    description='A pure Python 2/3 library for peripheral I/O (GPIO, SPI, I2C, MMIO, PWM, Serial) in Linux.',
     author='vsergeev',
     author_email='vsergeev at gmail',
     url='https://github.com/vsergeev/python-periphery',
     packages=['periphery'],
-    long_description="""python-periphery is a pure Python library for GPIO, SPI, I2C, MMIO, and Serial peripheral I/O interface access in userspace Linux. It is useful in embedded Linux environments (including BeagleBone, Raspberry Pi, etc. platforms) for interfacing with external peripherals. python-periphery is compatible with Python 2 and Python 3, is written in pure Python, and is MIT licensed. See https://github.com/vsergeev/python-periphery for more information.""",
+    long_description="""python-periphery is a pure Python library for GPIO, SPI, I2C, MMIO, PWM, and Serial peripheral I/O interface access in userspace Linux. It is useful in embedded Linux environments (including BeagleBone, Raspberry Pi, etc. platforms) for interfacing with external peripherals. python-periphery is compatible with Python 2 and Python 3, is written in pure Python, and is MIT licensed. See https://github.com/vsergeev/python-periphery for more information.""",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
@@ -23,6 +23,6 @@ setup(
         'Topic :: System :: Hardware :: Hardware Drivers',
     ],
     license='MIT',
-    keywords='gpio spi i2c mmio serial uart embedded linux beaglebone raspberrypi rpi',
+    keywords='gpio spi i2c mmio pwm serial uart embedded linux beaglebone raspberrypi rpi odroid',
     )
 
