70e3ad47470eac5c2129510d6c78f5c15da79e7b
Vanya A. Sergeev <v@sergeev.io>
2018-04-03 23:51:27 -0500

    init: improve formatting for pep8

--- periphery/__init__.py
+++ periphery/__init__.py
@@ -1,10 +1,11 @@
+import time
+
 __version__ = "1.1.0"
 "Module version string."
 
-version = (1,1,0)
+version = (1, 1, 0)
 "Module version tuple."
 
-import time
 
 def sleep(seconds):
     """Sleep for the specified number of seconds.
@@ -15,6 +16,7 @@ def sleep(seconds):
     """
     time.sleep(seconds)
 
+
 def sleep_ms(milliseconds):
     """Sleep for the specified number of milliseconds.
 
@@ -23,6 +25,7 @@ def sleep_ms(milliseconds):
 
     """
     time.sleep(milliseconds / 1000.0)
+
 
 def sleep_us(microseconds):
     """Sleep for the specified number of microseconds.
@@ -33,6 +36,7 @@ def sleep_us(microseconds):
     """
     time.sleep(microseconds / 1000000.0)
 
+
 from periphery.gpio import GPIO, GPIOError
 from periphery.led import LED, LEDError
 from periphery.pwm import PWM, PWMError
@@ -40,4 +44,3 @@ from periphery.spi import SPI, SPIError
 from periphery.i2c import I2C, I2CError
 from periphery.mmio import MMIO, MMIOError
 from periphery.serial import Serial, SerialError
-
