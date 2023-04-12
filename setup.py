97fee0c59bb001d857b2643109a218ab1191495c
Juho Kim <juho-kim@outlook.com>
2023-04-12 17:49:29 -0400

    Add typestubs

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     author_email='v@sergeev.io',
     url='https://github.com/vsergeev/python-periphery',
     packages=['periphery'],
+    package_data={'periphery': ['*.pyi', 'py.typed']},
     long_description="""python-periphery is a pure Python library for GPIO, LED, PWM, SPI, I2C, MMIO, and Serial peripheral I/O interface access in userspace Linux. It is useful in embedded Linux environments (including Raspberry Pi, BeagleBone, etc. platforms) for interfacing with external peripherals. python-periphery is compatible with Python 2 and Python 3, is written in pure Python, and is MIT licensed. See https://github.com/vsergeev/python-periphery for more information.""",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
